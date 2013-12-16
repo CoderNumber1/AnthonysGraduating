6f2a5c81ea6650e13771031524a837c5d3293e66
Anthony James <karl.a.james@hotmail.com>
2013-12-16 12:49:12 -0600

    Pre-restore work.

--- AnthonysGraduating/Controllers/HomeController.cs
+++ AnthonysGraduating/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SendGrid.Transport;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,10 +18,26 @@ namespace AnthonysGraduating.Controllers
 
         public ActionResult Directions()
         {
+            //SendGrid.Mail message = SendGrid.Mail.GetInstance();
+            //MailAddress from = new MailAddress("noreply@anthonysgraduating.com");
+            //message.From = from;
+            //message.AddTo("karl.a.james@hotmail.com");
+            //message.Html = "<p>Hello World</p>";
+            //message.Subject = "SendGrid Hello World";
+
+            //var credentials = new NetworkCredential("azure_886476f1273c52bd47c9c1185033337a@azure.com", "ormyen5i");
+            //var transport = SMTP.GetInstance(credentials);
+            //transport.Deliver(message);
+
             return View();
         }
 
         public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult CommencementOnline()
         {
             return View();
         }
--- AnthonysGraduating/Global.asax.cs
+++ AnthonysGraduating/Global.asax.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using AnthonysGraduating.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -11,6 +16,26 @@ namespace AnthonysGraduating
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        async Task<bool> AddRoleAndUser()
+        {
+            UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            RoleManager<IdentityRole> IdentityManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+            if (!await IdentityManager.RoleExistsAsync("Admin"))
+            {
+                var adminRole = new IdentityRole("Admin");
+                IdentityResult adminCreateResult = await IdentityManager.CreateAsync(adminRole);
+            }
+
+            var adminUser = new ApplicationUser() { UserName = "Anthony" };
+            var adminUserCreateResult = await userManager.CreateAsync(adminUser, "GradPower135");
+
+
+            var adminUserAddToRoleResult = await userManager.AddToRoleAsync(adminUser.Id, "Admin");
+
+            return true;
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -18,6 +43,8 @@ namespace AnthonysGraduating
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AddRoleAndUser();
         }
     }
 }
